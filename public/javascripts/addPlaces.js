0a79e6ec8f7cb8b0e97c8b3d12b56d1732013f61
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-11-26 11:00:27 +0100

    drop down mejorado

--- public/javascripts/addPlaces.js
+++ public/javascripts/addPlaces.js
@@ -2,6 +2,7 @@ document.addEventListener(
   "DOMContentLoaded",
   () => {
 
+  
     const mapAddPlaces = new google.maps.Map(document.getElementById('mapAddPlaces'), {
       zoom: 18,
     });
@@ -10,33 +11,32 @@ document.addEventListener(
       mapAddPlaces.setCenter(center);
     });
 
-    // const setPosOnForm = (latlng) => {
-    //   document.getElementById('lat-pos').value = latlng.lat;
-    //   document.getElementById('lng-pos').value = latlng.lng;
+    const setPosOnForm = (latlng) => {
+      document.getElementById('lat-pos').value = latlng.lat;
+      document.getElementById('lng-pos').value = latlng.lng;
+    }
   
-    // }
+    let marker;
   
-    // let marker;
+    mapAddPlaces.addListener('click', function(e) {
+      const clickPos = {
+        lat:e.latLng.lat(),
+        lng:e.latLng.lng()
+      }
+      console.log(clickPos);
+      marker.setPosition(clickPos);
+      setPosOnForm(clickPos)
+    });
   
-    // map.addListener('click', function(e) {
-    //   const clickPos = {
-    //     lat:e.latLng.lat(),
-    //     lng:e.latLng.lng()
-    //   }
-    //   console.log(clickPos);
-    //   marker.setPosition(clickPos);
-    //   setPosOnForm(clickPos)
-    // });
+    geolocalize().then(center => {
+      mapAddPlaces.setCenter(center);
+      marker = new google.maps.Marker({
+        position: center,
+        map: mapAddPlaces
+      });
+      setPosOnForm(center);
+    });
   
-    // geolocalize().then(center => {
-    //   map.setCenter(center);
-    //   marker = new google.maps.Marker({
-    //     position: center,
-    //     map: map
-    //   });
-    //   setPosOnForm(center);
-    // });
-
   },
   false
 );
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const router  = express.Router();
-
+const Place = require("../models/Place")
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
@@ -11,11 +11,20 @@ router.get("/addplace", (req, res, next) => {
   res.render("add/place");
 });
 
-// router.post("/login", passport.authenticate("local", {
-//   successRedirect: "/",
-//   failureRedirect: "/auth/login",
-//   failureFlash: true,
-//   passReqToCallback: true
-// }));
+router.post('/addplace', (req, res, next) => {
+
+  let currentPlace = {
+    name: req.body.name,
+    description: req.body.description,
+    location: {
+      type: 'Point',
+      coordinates: [Number(req.body.latitude), Number(req.body.longitude)]
+    }
+  }
+  console.log(currentPlace);
+  Place.create(currentPlace).then( currentPlace => {
+    res.redirect('/');
+  }).catch(e=> next(e));
+});
 
 module.exports = router;
